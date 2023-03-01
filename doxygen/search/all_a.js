162c9ea8c86f83d0c7226c9c83622614643335d4
Masaki Murooka <m-murooka@aist.go.jp>
2023-03-01 05:30:48 +0000

    Update Doxygen HTML files from commit 147946f08b4005b5af647d5c60698eb24070f912

--- doxygen/search/all_a.js
+++ doxygen/search/all_a.js
@@ -6,6 +6,6 @@ var searchData=
   ['resultwrenchratio_5f_33',['resultWrenchRatio_',['../classForceColl_1_1WrenchDistribution.html#a86e4743670affc6efd90a91514dea14d',1,'ForceColl::WrenchDistribution']]],
   ['ridgeforceminmax_34',['ridgeForceMinMax',['../structForceColl_1_1WrenchDistribution_1_1Configuration.html#ac6f972bc5ee3eb09f92352f7363c44ef',1,'ForceColl::WrenchDistribution::Configuration']]],
   ['ridgelist_35',['ridgeList',['../structForceColl_1_1Contact_1_1VertexWithRidge.html#a88d8c24418d3eb2192d76ca2ef45e543',1,'ForceColl::Contact::VertexWithRidge']]],
-  ['ridgenum_36',['ridgeNum',['../classForceColl_1_1FrictionPyramid.html#a28df1f3a38092cc5fc544b74092ade09',1,'ForceColl::FrictionPyramid']]],
+  ['ridgenum_36',['ridgeNum',['../classForceColl_1_1FrictionPyramid.html#a28df1f3a38092cc5fc544b74092ade09',1,'ForceColl::FrictionPyramid::ridgeNum()'],['../classForceColl_1_1Contact.html#a7c2c5d381af0b711bef88ca3910863d9',1,'ForceColl::Contact::ridgeNum()']]],
   ['run_37',['run',['../classForceColl_1_1WrenchDistribution.html#a4c7ec653c4965e5e3ea1c6503265d056',1,'ForceColl::WrenchDistribution']]]
 ];
--- doxygen/search/functions_7.js
+++ doxygen/search/functions_7.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['ridgenum_76',['ridgeNum',['../classForceColl_1_1FrictionPyramid.html#a28df1f3a38092cc5fc544b74092ade09',1,'ForceColl::FrictionPyramid']]],
+  ['ridgenum_76',['ridgeNum',['../classForceColl_1_1FrictionPyramid.html#a28df1f3a38092cc5fc544b74092ade09',1,'ForceColl::FrictionPyramid::ridgeNum()'],['../classForceColl_1_1Contact.html#a7c2c5d381af0b711bef88ca3910863d9',1,'ForceColl::Contact::ridgeNum()']]],
   ['run_77',['run',['../classForceColl_1_1WrenchDistribution.html#a4c7ec653c4965e5e3ea1c6503265d056',1,'ForceColl::WrenchDistribution']]]
 ];
